91273d1cdc5f85ee4b7c002b60397bdd1865ba1a
Ko-Miyama <chinpanzi914@gmail.com>
2022-08-10 16:17:08 +0900

    マイグレーション

---
+++ database/migrations/2022_08_10_060119_create_submits_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSubmitsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('submits', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('file_path', 20);
+            $table->string('manner', 20);
+            $table->string('comment', 100);
+            $table->decimal('score', 5, 2);
+            $table->timestamps();
+            $table->softDeletes();
+            $table->integer('user_id')->unsigned();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('submits');
+    }
+}
